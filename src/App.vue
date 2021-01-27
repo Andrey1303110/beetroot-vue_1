7eab9325285ddf56e47c812ad2e2e67b078755d6
Andrey1303110 <andrey1303110@gmail.com>
2021-01-27 20:35:50 +0200

    commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <input @keyup="showMessage($event)" />
+  <h1>{{ message }}</h1>
+  <p v-text="message"></p>
+
+  <div v-html="link"></div>
+
+  <Counter color="red" />
+
+  <Counter color="green" :counter="initialValue" />
+
+  <Counter color="blue" counter="2" />
+
+  <Counter color="orange" counter="3" />
+
+  <Counter color="purple" counter="-7" />
+</template>
+
+<script>
+import Counter from "./components/Counter";
+
+export default {
+  components: {
+    Counter
+  },
+  data() {
+    return {
+      message: "Hello World!!!",
+      link: `<a href="http://www.google.com">Google</a>`,
+    }
+  },
+  methods: {
+    showMessage(event) {
+      this.message = event.target.value;
+    }
+  },
+  computed: {
+    initialValue() {
+      return 7 * 7;
+    },
+  },
+
+}
+</script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+
+    <h1 :style="`color: ${color}`">{{ counter }}</h1>
+    <button @click="counter--">-</button><button @click="counter++">+</button>
+
+  </div>  
+</template>
+
+<script>
+export default {
+  props: {
+    counter: {
+      type: Number,
+      default: 0
+    },
+    color: String,
+  },
+  /*data() {
+    return {
+      counter: 0,
+    }
+  }*/
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+  display: inline;
+}
+</style>
